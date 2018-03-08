dd164e492738887bac069852b4481249eed4d182
Tony.Reina <greina@eng.ucsd.edu>
2018-03-07 17:01:20 -0800

    The tranpose 3D conv op in tf.keras seems to be broken. The one in the keras API is working. Have switched to just using Keras API for the model definition.

--- 3D_UNet/model.py
+++ 3D_UNet/model.py
@@ -18,6 +18,7 @@
 import os.path
 import numpy as np
 import tensorflow as tf
+import keras
 
 def dice_coef(target, prediction, axis=(1, 2, 3), smooth=1e-5):
 	'''
@@ -49,13 +50,11 @@ def dice_coef_loss(target, prediction, axis=(1,2,3), smooth=1e-5):
 CHANNEL_LAST = True
 if CHANNEL_LAST:
 	concat_axis = -1
-	data_format = 'channels_last'
+	data_format = "channels_last"
 
 else:
 	concat_axis = 1
-	data_format = 'channels_first'
-
-tf.keras.backend.set_image_data_format(data_format)
+	data_format = "channels_first"
 
 def is_power_of_2(num):
 	'''
@@ -78,67 +77,68 @@ def define_model(input_img, use_upsampling=False, learning_rate=0.001, n_cl_out=
 	# 	not is_power_of_2(d):
 	# 	print("ERROR: Image dimension lengths must be a power of 2. e.g. 16x256x32")
 
-	# inputs = tf.keras.layers.Input(shape=(h,w,d,c), name="Input_Image")
+	# inputs = keras.layers.Input(shape=(h,w,d,c), name="Input_Image")
 
 	# Set keras learning phase to train
-	tf.keras.backend.set_learning_phase(True)
+	keras.backend.set_learning_phase(True)
 
 	# Don't initialize variables on the fly
-	tf.keras.backend.manual_variable_initialization(False)
+	keras.backend.manual_variable_initialization(False)
 
-	inputs = tf.keras.layers.Input(tensor=input_img, name="Input_Image")
+	inputs = keras.layers.Input(tensor=input_img, name="Input_Image")
 
 	params = dict(kernel_size=(3, 3, 3), activation="relu",
 				  padding="same", data_format=data_format,
 				  kernel_initializer="he_uniform")
 
-	conv1 = tf.keras.layers.Conv3D(name="conv1a", filters=32, **params)(inputs)
-	conv1 = tf.keras.layers.Conv3D(name="conv1b", filters=64, **params)(conv1)
-	pool1 = tf.keras.layers.MaxPooling3D(name="pool1", pool_size=(2, 2, 2))(conv1)
+	conv1 = keras.layers.Conv3D(name="conv1a", filters=32, **params)(inputs)
+	conv1 = keras.layers.Conv3D(name="conv1b", filters=64, **params)(conv1)
+	pool1 = keras.layers.MaxPooling3D(name="pool1", pool_size=(2, 2, 2))(conv1)
 
-	conv2 = tf.keras.layers.Conv3D(name="conv2a", filters=64, **params)(pool1)
-	conv2 = tf.keras.layers.Conv3D(name="conv2b", filters=128, **params)(conv2)
-	pool2 = tf.keras.layers.MaxPooling3D(name="pool2", pool_size=(2, 2, 2))(conv2)
+	conv2 = keras.layers.Conv3D(name="conv2a", filters=64, **params)(pool1)
+	conv2 = keras.layers.Conv3D(name="conv2b", filters=128, **params)(conv2)
+	pool2 = keras.layers.MaxPooling3D(name="pool2", pool_size=(2, 2, 2))(conv2)
 
-	conv3 = tf.keras.layers.Conv3D(name="conv3a", filters=128, **params)(pool2)
-	conv3 = tf.keras.layers.Dropout(dropout)(conv3) ### Trying dropout layers earlier on, as indicated in the paper
-	conv3 = tf.keras.layers.Conv3D(name="conv3b", filters=256, **params)(conv3)
-	pool3 = tf.keras.layers.MaxPooling3D(name="pool3", pool_size=(2, 2, 2))(conv3)
+	conv3 = keras.layers.Conv3D(name="conv3a", filters=128, **params)(pool2)
+	conv3 = keras.layers.Dropout(dropout)(conv3) ### Trying dropout layers earlier on, as indicated in the paper
+	conv3 = keras.layers.Conv3D(name="conv3b", filters=256, **params)(conv3)
+	pool3 = keras.layers.MaxPooling3D(name="pool3", pool_size=(2, 2, 2))(conv3)
 
-	conv4 = tf.keras.layers.Conv3D(name="conv4a", filters=256, **params)(pool3)
-	conv4 = tf.keras.layers.Dropout(dropout)(conv4) ### Trying dropout layers earlier on, as indicated in the paper
-	conv4 = tf.keras.layers.Conv3D(name="conv4b", filters=512, **params)(conv4)
+	conv4 = keras.layers.Conv3D(name="conv4a", filters=256, **params)(pool3)
+	conv4 = keras.layers.Dropout(dropout)(conv4) ### Trying dropout layers earlier on, as indicated in the paper
+	conv4 = keras.layers.Conv3D(name="conv4b", filters=512, **params)(conv4)
 
 	if use_upsampling:
-		up4 = tf.keras.layers.concatenate([tf.keras.layers.UpSampling3D(name="up4", size=(2, 2, 2))(conv4), conv3], axis=concat_axis)
+		up4 = keras.layers.concatenate([keras.layers.UpSampling3D(name="up4", size=(2, 2, 2))(conv4), conv3], axis=concat_axis)
 	else:
-		up4 = tf.keras.layers.concatenate([tf.keras.layers.Conv3DTranspose(name="transConv4", filters=512, data_format=data_format,
+		up4 = keras.layers.concatenate([keras.layers.Conv3DTranspose(name="transConv4", filters=512, data_format=data_format,
 						   kernel_size=(2, 2, 2), strides=(2, 2, 2), padding="same")(conv4), conv3], axis=concat_axis)
 
-	conv5 = tf.keras.layers.Conv3D(name="conv5a", filters=256, **params)(up4)
-	conv5 = tf.keras.layers.Conv3D(name="conv5b", filters=256, **params)(conv5)
+
+	conv5 = keras.layers.Conv3D(name="conv5a", filters=256, **params)(up4)
+	conv5 = keras.layers.Conv3D(name="conv5b", filters=256, **params)(conv5)
 
 	if use_upsampling:
-		up5 = tf.keras.layers.concatenate([tf.keras.layers.UpSampling3D(name="up5", size=(2, 2, 2))(conv5), conv2], axis=concat_axis)
+		up5 = keras.layers.concatenate([keras.layers.UpSampling3D(name="up5", size=(2, 2, 2))(conv5), conv2], axis=concat_axis)
 	else:
-		up5 = tf.keras.layers.concatenate([tf.keras.layers.Conv3DTranspose(name="transConv5", filters=256, data_format=data_format,
+		up5 = keras.layers.concatenate([keras.layers.Conv3DTranspose(name="transConv5", filters=256, data_format=data_format,
 						   kernel_size=(2, 2, 2), strides=(2, 2, 2), padding="same")(conv5), conv2], axis=concat_axis)
 
-	conv6 = tf.keras.layers.Conv3D(name="conv6a", filters=128, **params)(up5)
-	conv6 = tf.keras.layers.Conv3D(name="conv6b", filters=128, **params)(conv6)
+	conv6 = keras.layers.Conv3D(name="conv6a", filters=128, **params)(up5)
+	conv6 = keras.layers.Conv3D(name="conv6b", filters=128, **params)(conv6)
 
 	if use_upsampling:
-		up6 = tf.keras.layers.concatenate([tf.keras.layers.UpSampling3D(name="up6", size=(2, 2, 2))(conv6), conv1], axis=concat_axis)
+		up6 = keras.layers.concatenate([keras.layers.UpSampling3D(name="up6", size=(2, 2, 2))(conv6), conv1], axis=concat_axis)
 	else:
-		up6 = tf.keras.layers.concatenate([tf.keras.layers.Conv3DTranspose(name="transConv6", filters=128, data_format=data_format,
+		up6 = keras.layers.concatenate([keras.layers.Conv3DTranspose(name="transConv6", filters=128, data_format=data_format,
 						   kernel_size=(2, 2, 2), strides=(2, 2, 2), padding="same")(conv6), conv1], axis=concat_axis)
 
-	conv7 = tf.keras.layers.Conv3D(name="conv7a", filters=128, **params)(up6)
-	conv7 = tf.keras.layers.Conv3D(name="conv7b", filters=128, **params)(conv7)
-	pred = tf.keras.layers.Conv3D(name="Prediction_Mask", filters=n_cl_out, kernel_size=(1, 1, 1),
+	conv7 = keras.layers.Conv3D(name="conv7a", filters=128, **params)(up6)
+	conv7 = keras.layers.Conv3D(name="conv7b", filters=128, **params)(conv7)
+	pred = keras.layers.Conv3D(name="Prediction_Mask", filters=n_cl_out, kernel_size=(1, 1, 1),
 					data_format=data_format, activation="sigmoid")(conv7)
 
-	model = tf.keras.models.Model(inputs=[inputs], outputs=[pred])
+	model = keras.models.Model(inputs=[inputs], outputs=[pred])
 
 	if print_summary:
 		model.summary()
