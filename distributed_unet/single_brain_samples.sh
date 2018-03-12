a191c594fb8d11f9ac54faa9aa9f0ba6ebb4dc02
mattsonthieme <mattson.thieme@gmail.com>
2018-03-12 10:51:26 -0700

    Add training data back; add train_test_split to single_brain_samples.sh

--- distributed_unet/single_brain_samples.sh
+++ distributed_unet/single_brain_samples.sh
@@ -6,19 +6,20 @@
 data_dir='/home/bduser/data_test/MICCAI_BraTS17_Data_Training/'
 section='HGG/'
 sample_dir='/home/bduser/data_test/'
+train_test_split=0.80 # percent of the dataset to use for training
 declare -i fin_msk_size=128 # must be 128 to work with current U-Net
 declare -i fin_img_size=128 # can be larger than 128 if desired, is re-made after inference
 
 source activate tf
 
 # Add scans in list below to generate single brain samples
-for sample in 'Brats17_TCIA_606_1' 'Brats17_TCIA_607_1'; do
+for sample in 'Brats17_TCIA_296_1' 'Brats17_TCIA_607_1'; do
 
         cp -r $data_dir$section$sample $sample_dir
 
         mkdir $sample_dir$sample/data
 
-        time python sample_converter.py $data_dir $sample $fin_msk_size $sample_dir$sample/data/
+        time python sample_converter.py $data_dir $sample $train_test_split $fin_msk_size $sample_dir$sample/data/
 
         time python sample_sanity_check.py $sample_dir$sample/data/
 
@@ -26,9 +27,7 @@ for sample in 'Brats17_TCIA_606_1' 'Brats17_TCIA_607_1'; do
 
         rm $sample_dir$sample/data/*train.npy
 
-        time python sample_converter.py $data_dir $sample $fin_img_size $sample_dir$sample/data/
-
-        rm $sample_dir$sample/data/*train.npy
+        time python sample_converter.py $data_dir $sample $train_test_split $fin_img_size $sample_dir$sample/data/
 
 done
 
