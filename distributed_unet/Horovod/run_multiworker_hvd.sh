94ec58313c96b52fe88e32902327d04a998ed5d2
Tony Reina <greina@eng.ucsd.edu>
2018-06-12 18:13:05 -0700

    Update run_multiworker_hvd.sh

--- distributed_unet/Horovod/run_multiworker_hvd.sh
+++ distributed_unet/Horovod/run_multiworker_hvd.sh
@@ -1,3 +1,3 @@
 bash synch_servers.sh
 export physical_cores=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"`
-mpirun -np 12 --hostfile hosts.txt -bind-to none --map-by ppr:2:socket:pe=physical_cores bash hvd_multiworker.sh _multiworker
+mpirun -np 12 --hostfile hosts.txt -bind-to none --map-by ppr:2:socket:pe=$physical_cores bash hvd_multiworker.sh _multiworker
