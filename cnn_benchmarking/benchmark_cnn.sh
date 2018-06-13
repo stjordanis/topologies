394bc5842604772c4a76d070d113f9322f754b0b
Tony Reina <greina@eng.ucsd.edu>
2018-06-12 18:25:24 -0700

    Update benchmark_cnn.sh

--- cnn_benchmarking/benchmark_cnn.sh
+++ cnn_benchmarking/benchmark_cnn.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-git clone -b mkl_experiment https://github.com/tensorflow/benchmarks.git
+optimized=${1:-True}
+
+if [ $optimized = True ]; then
+   git clone -b mkl_experiment https://github.com/tensorflow/benchmarks.git
+   
+   export KMP_AFFINITY=granularity=fine,compact,1,0
+   export KMP_BLOCKTIME=1
+   export KMP_SETTINGS=1
+   export OMP_PROC_BIND=true
+   
+   export bz1_num_cores=8
+   export num_cores=`grep -c ^processor /proc/cpuinfo`
+
+else
+   git clone https://github.com/tensorflow/benchmarks.git
+fi
+
 cd benchmarks/scripts/tf_cnn_benchmarks
 
 sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
 set -x
-
-export KMP_AFFINITY=granularity=fine,compact,1,0
-export KMP_BLOCKTIME=1
-export KMP_SETTINGS=1
-export OMP_PROC_BIND=true
 
 date > start_benchmark.txt
 
@@ -17,9 +28,8 @@ date > start_benchmark.txt
 for network in googlenet inception3 resnet50 resnet152 ; do
 
     inter=2
-    num_cores=8
     bz=1
-    OMP_NUM_THREADS=$num_cores
+    OMP_NUM_THREADS=$bz1_num_cores
     echo -e "\n\n #### Starting $network and BZ=$bz ####\n\n"
 
     python tf_cnn_benchmarks.py --device cpu --forward_only True \
@@ -32,7 +42,6 @@ for network in googlenet inception3 resnet50 resnet152 ; do
 
     echo -e "#### Finished $network and BZ=$bz ####"
 
-    num_cores=`grep -c ^processor /proc/cpuinfo`
     OMP_NUM_THREADS=$num_cores
 
     for bz in 32 64 96 128; do
@@ -56,7 +65,7 @@ for network in vgg16 ; do
     inter=1
     num_cores=8
     bz=1
-    OMP_NUM_THREADS=$num_cores
+    OMP_NUM_THREADS=$bz1_num_cores
     echo -e "\n\n #### Starting $network and BZ=$bz ####\n\n"
 
     python tf_cnn_benchmarks.py --device cpu --forward_only True \
@@ -69,7 +78,6 @@ for network in vgg16 ; do
 
     echo -e "#### Finished $network and BZ=$bz ####"
 
-    num_cores=`grep -c ^processor /proc/cpuinfo`
     OMP_NUM_THREADS=$num_cores
 
     for bz in 32 64 96 128; do
