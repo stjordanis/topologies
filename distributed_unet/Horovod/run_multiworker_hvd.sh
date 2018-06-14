34eb30f6ca88312dfcff631bb51b10564040d530
mattsonthieme <mattson.thieme@gmail.com>
2018-06-14 18:30:04 +0100

    Update run command with calculated values for number of processes, inter and intra op threads to pass to hvd_train.py

--- distributed_unet/Horovod/run_multiworker_hvd.sh
+++ distributed_unet/Horovod/run_multiworker_hvd.sh
@@ -1,3 +1,14 @@
 bash synch_servers.sh
+
+export num_nodes=`wc -l < hosts.txt`
+export num_workers_per_node=4  #TensorFlow instances per node
+export num_processes=$(( $num_nodes * $num_workers_per_node ))
+export ppr=2   # Two processes per resource (e.g. socket)
 export physical_cores=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"`
-mpirun -np 12 --hostfile hosts.txt -bind-to none --map-by ppr:2:socket:pe=$physical_cores bash hvd_multiworker.sh _multiworker
+export pe=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"` #$(( $physical_cores / $ppr ))
+export num_threads=$(( $ppr * $physical_cores ))
+
+mpirun -np $num_processes --hostfile hosts.txt -bind-to none \
+        --map-by ppr:$ppr:socket:pe=$pe \
+	--report-bindings --oversubscribe \
+        bash hvd_multiworker.sh _multiworker $ppr $num_threads
