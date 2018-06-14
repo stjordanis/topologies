e188e447984565288d50e67c3037ba805fbb892f
mattsonthieme <mattson.thieme@gmail.com>
2018-06-14 21:30:11 +0100

    Add second and third arguments for inter/intra op threads

--- distributed_unet/Horovod/hvd_singleworker.sh
+++ distributed_unet/Horovod/hvd_singleworker.sh
@@ -1,3 +1,4 @@
 source activate tf
-python hvd_train.py --epochs=15 --learningrate=0.0005 --logdir="tensorboard${1}"
+export OMP_NUM_THREADS=${3}
+python hvd_train.py --epochs=15 --learningrate=0.0005 --logdir="tensorboard${1}" --num_inter_threads=${2} --num_threads=${3}
 source deactivate
