b8acaf870e5f92151d2622270ee2271dc1a40695
mattsonthieme <mattson.thieme@gmail.com>
2018-06-14 21:30:42 +0100

    Update singleworker run command

--- distributed_unet/Horovod/run_singleworker_hvd.sh
+++ distributed_unet/Horovod/run_singleworker_hvd.sh
@@ -1,2 +1,7 @@
 bash synch_servers.sh
-mpirun -np 3 -H 192.168.5.152:1,192.168.5.153:1,192.168.5.154:1 -bind-to none -map-by slot bash hvd_singleworker.sh _singleworker
+
+export num_nodes=3
+export inter_op=2
+export physical_cores=48
+
+mpirun -np $num_nodes -H 192.168.5.152:1,192.168.5.153:1,192.168.5.154:1 -map-by slot -bind-to none bash hvd_singleworker.sh _singleworker $inter_op $physical_cores
