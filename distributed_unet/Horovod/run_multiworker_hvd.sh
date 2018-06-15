9ac2e079818273c35ed62c0b78e018917a35d8aa
mattsonthieme <mattson.thieme@gmail.com>
2018-06-15 18:59:08 +0100

    Disambiguate inter-op threads from ppr

--- distributed_unet/Horovod/run_multiworker_hvd.sh
+++ distributed_unet/Horovod/run_multiworker_hvd.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-# To run: bash run_multiworker_hvd.sh <logidr> <hostfile> <workers per node>
+# To run: bash run_multiworker_hvd.sh <logidr> <hostfile> <workers per node> <inter op threads>
 # Note: The total number of workers deployed will be the number of workers per node * number of nodes
 
 logdir=${1:-_multiworker}     # Default suffix is _multiworker
 node_ips=${2:-hosts.txt}      # Default is the hosts.txt file
 export num_workers_per_node=${3:-4}  # Default 4 workers per node
+export num_inter_threads=${4:-2} # Default to 2 inter_op threads
 
 export num_nodes=`cat hosts.txt | sed '/^\s*$/d' | wc -l` # Hosts.txt should contain a single host per line
 export num_processes=$(( $num_nodes * $num_workers_per_node )) # Total number of workers across all nodes
 export ppr=2   # Number of sockets per node
-export physical_cores=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"` # Total number of physical cores per socket
-export num_threads=$(( $ppr * $physical_cores )) # Total number of physical cores on the machine
 
-echo "Running $num_workers_per_node workers on $num_nodes nodes..."
+echo "Running $num_workers_per_node worker(s)/node on $num_nodes nodes..."
 
 echo "Synching hosts.."
 bash synch_servers.sh
@@ -20,4 +19,4 @@ bash synch_servers.sh
 mpirun -np $num_processes --hostfile $node_ips -bind-to none \
         --map-by ppr:$ppr:socket:pe=$physical_cores \
 	--report-bindings --oversubscribe \
-        bash hvd_multiworker.sh $logdir $ppr $num_threads
+        bash exec_multiworker.sh $logdir $num_inter_threads
--- distributed_unet/Horovod/run_singleworker_hvd.sh
+++ distributed_unet/Horovod/run_singleworker_hvd.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+# To run: bash run_singleworker_hvd.sh <logidr> <hostfile> <workers per node> <inter op threads>
+# Note: The total number of workers deployed will be the number of workers per node * number of nodes
+
 logdir=${1:-_singleworker}     # Default suffix is _singeworker
 node_ips=${2:-hosts.txt}      # Default is the hosts.txt file
 export num_workers_per_node=${3:-1}  # Default 1 worker per node
+export num_inter_threads=${4:-2} # Default to 2 inter_op threads
 
 export num_nodes=`cat hosts.txt | sed '/^\s*$/d' | wc -l` # Hosts.txt should contain a single host per line
 export num_processes=$(( $num_nodes * $num_workers_per_node ))
@@ -9,7 +14,7 @@ export physical_cores=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "
 export pe=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"` #$(( $physical_cores / $ppr ))
 export num_threads=$(( $ppr * $physical_cores ))
 
-echo "Running $num_workers_per_node worker(s) on $num_nodes nodes..."
+echo "Running $num_workers_per_node worker(s)/node on $num_nodes nodes..."
 
 echo "Synching hosts.."
 bash synch_servers.sh
@@ -17,4 +22,4 @@ bash synch_servers.sh
 mpirun -np $num_processes --hostfile $node_ips -bind-to none \
         --map-by ppr:$ppr:socket:pe=$pe \
 	--report-bindings --oversubscribe \
-        bash hvd_singleworker.sh $logdir $ppr $num_threads
+        bash exec_singleworker.sh $logdir $num_inter_threads
