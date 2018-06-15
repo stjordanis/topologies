1f4b39deaf5baf616ed7664900ac085817a6dc1d
mattsonthieme <mattson.thieme@gmail.com>
2018-06-15 02:54:06 +0100

    modified bash scripts for single and multi worker

--- distributed_unet/Horovod/run_multiworker_hvd.sh
+++ distributed_unet/Horovod/run_multiworker_hvd.sh
@@ -1,16 +1,17 @@
-logdir=${1:-_multiworker}
+logdir=${1:-_multiworker}     # Default suffix is _multiworker
+node_ips=${2:-hosts.txt}      # Default is the hosts.txt file
+export num_workers_per_node=${3:-4}  # Default 4 workers per node
 
-bash synch_servers.sh
-
-export num_nodes=`wc -l < hosts.txt`
-export num_workers_per_node=4  #TensorFlow instances per node
+export num_nodes=`wc -l < ${node_ips}`
 export num_processes=$(( $num_nodes * $num_workers_per_node ))
 export ppr=2   # Two processes per resource (e.g. socket)
 export physical_cores=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"`
 export pe=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"` #$(( $physical_cores / $ppr ))
 export num_threads=$(( $ppr * $physical_cores ))
 
-mpirun -np $num_processes --hostfile hosts.txt -bind-to none \
+bash synch_servers.sh
+
+mpirun -np $num_processes --hostfile $node_ips -bind-to none \
         --map-by ppr:$ppr:socket:pe=$pe \
 	--report-bindings --oversubscribe \
         bash hvd_multiworker.sh $logdir $ppr $num_threads
--- distributed_unet/Horovod/run_singleworker_hvd.sh
+++ distributed_unet/Horovod/run_singleworker_hvd.sh
@@ -1,8 +1,17 @@
-logdir=${1:-_singleworker}
+logdir=${1:-_singleworker}     # Default suffix is _singeworker
+node_ips=${2:-hosts.txt}      # Default is the hosts.txt file
+export num_workers_per_node=${3:-1}  # Default 1 worker per node
+
+export num_nodes=`wc -l < ${node_ips}`
+export num_processes=$(( $num_nodes * $num_workers_per_node ))
+export ppr=2   # Two processes per resource (e.g. socket)
+export physical_cores=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"`
+export pe=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"` #$(( $physical_cores / $ppr ))
+export num_threads=$(( $ppr * $physical_cores ))
+
 bash synch_servers.sh
 
-export num_nodes=3
-export inter_op=2
-export physical_cores=48
-
-mpirun -np $num_nodes -H 192.168.5.152:1,192.168.5.153:1,192.168.5.154:1 -map-by slot -bind-to none bash hvd_singleworker.sh $logdir $inter_op $physical_cores
+mpirun -np $num_processes --hostfile $node_ips -bind-to none \
+        --map-by ppr:$ppr:socket:pe=$pe \
+	--report-bindings --oversubscribe \
+        bash hvd_singleworker.sh $logdir $ppr $num_threads
