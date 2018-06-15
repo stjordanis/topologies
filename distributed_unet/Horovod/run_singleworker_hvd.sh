a3c714462a8cc373fa99193159e291a3a13685ff
mattsonthieme <mattson.thieme@gmail.com>
2018-06-15 17:41:20 +0100

    Update line counting in hosts.txt; clarify roles of variables

--- distributed_unet/Horovod/run_singleworker_hvd.sh
+++ distributed_unet/Horovod/run_singleworker_hvd.sh
@@ -2,13 +2,16 @@ logdir=${1:-_singleworker}     # Default suffix is _singeworker
 node_ips=${2:-hosts.txt}      # Default is the hosts.txt file
 export num_workers_per_node=${3:-1}  # Default 1 worker per node
 
-export num_nodes=`wc -l < ${node_ips}`
+export num_nodes=`cat hosts.txt | sed '/^\s*$/d' | wc -l` # Hosts.txt should contain a single host per line
 export num_processes=$(( $num_nodes * $num_workers_per_node ))
 export ppr=2   # Two processes per resource (e.g. socket)
 export physical_cores=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"`
 export pe=`lscpu | grep "Core(s) per socket" | cut -d':' -f2 | sed "s/ //g"` #$(( $physical_cores / $ppr ))
 export num_threads=$(( $ppr * $physical_cores ))
 
+echo "Running $num_workers_per_node worker(s) on $num_nodes nodes..."
+
+echo "Synching hosts.."
 bash synch_servers.sh
 
 mpirun -np $num_processes --hostfile $node_ips -bind-to none \
