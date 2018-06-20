9283eb8950e30763feebff2d707ecbd04beba80f
Tony Reina <g.anthony.reina@intel.com>
2018-06-20 02:55:48 +0100

    Corrected logic for non-gpu

--- memory_benchmarking/keras_only_benchmarking/run_3D_benchmarks.sh
+++ memory_benchmarking/keras_only_benchmarking/run_3D_benchmarks.sh
@@ -4,23 +4,29 @@ pip install memory_profiler
 rm *.dat 
 rm *.log
 
-using_gpu=${1:-True}
+using_gpu=${1:-False}
 
 for dim_length in 32 # 56 64 80 128 184 200 256
 do
 
    num=1000000  # Run for a long time because timeout will automatically stop the script after a certain number of seconds
-   secs=600  # Number of seconds to record memory
+   secs=30  # Number of seconds to record memory
 
    # Training batch size 1
    if [ $using_gpu == True ]; then
    	timeout 5 bash check_gpu_memory.sh > gpu_memory_${dim_length}_bz1_train.log
-   fi
-   timeout $secs mprof run benchmark_model.py --dim_length $dim_length --num_datapoints 5 --epochs $num --bz 1 \
-         2>&1 | tee train_unet_${dim_length}_bz1.log &
+   
+   	timeout $secs mprof run benchmark_model.py --dim_length $dim_length \
+  		 --num_datapoints 5 --epochs $num --bz 1 \
+        	 2>&1 | tee train_unet_${dim_length}_bz1.log &
 
-   if [ $using_gpu == True ]; then
    	timeout $secs bash check_gpu_memory.sh > gpu_memory_${dim_length}_bz1_train.log
+
+   else
+
+	timeout $secs mprof run benchmark_model.py --dim_length $dim_length \
+               	 --num_datapoints 5 --epochs $num --bz 1 \
+                 2>&1 | tee train_unet_${dim_length}_bz1.log
    fi
 
    pattern="mprofile_*.dat"
@@ -32,12 +38,17 @@ do
    # Training batch size 2
    if [ $using_gpu == True ]; then
    	timeout 5 bash check_gpu_memory.sh > gpu_memory_${dim_length}_bz2_train.log
-   fi
-   timeout $secs mprof run  benchmark_model.py --dim_length $dim_length --num_datapoints 5 --epochs $num --bz 2 \
-         2>&1 | tee train_unet_${dim_length}_bz2.log &
+  
+   	timeout $secs mprof run  benchmark_model.py --dim_length $dim_length \
+		--num_datapoints 5 --epochs $num --bz 2 \
+         	2>&1 | tee train_unet_${dim_length}_bz2.log &
 
-   if [ $using_gpu == True ]; then
    	timeout $secs bash check_gpu_memory.sh > gpu_memory_${dim_length}_bz2_train.log
+
+   else
+	timeout $secs mprof run  benchmark_model.py --dim_length $dim_length \
+               	--num_datapoints 5 --epochs $num --bz 2 \
+               	2>&1 | tee train_unet_${dim_length}_bz2.log
    fi
 
    pattern="mprofile_*.dat"
@@ -49,12 +60,16 @@ do
    # Inference batch size 1
    if [ $using_gpu == True ]; then
    	timeout 5 bash check_gpu_memory.sh > gpu_memory_${dim_length}_bz1_inference.log
-   fi
-   timeout $secs mprof run benchmark_model.py --dim_length $dim_length --inference --num_datapoints 5 --epochs $num --bz 1 \
-         2>&1 | tee inference_unet_${dim_length}_bz1.log &
+   
+   	timeout $secs mprof run benchmark_model.py --dim_length $dim_length \
+		--inference --num_datapoints 5 --epochs $num --bz 1 \
+        	 2>&1 | tee inference_unet_${dim_length}_bz1.log &
 
-   if [ $using_gpu == True ]; then
    	timeout $secs bash check_gpu_memory.sh > gpu_memory_${dim_length}_bz1_inference.log
+   else
+	timeout $secs mprof run benchmark_model.py --dim_length $dim_length \
+               	--inference --num_datapoints 5 --epochs $num --bz 1 \
+                 2>&1 | tee inference_unet_${dim_length}_bz1.log
    fi
 
    pattern="mprofile_*.dat"
