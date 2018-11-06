7bfed9afa23a4b5425011347e67cbb0bde40c833
Tony Reina <g.anthony.reina@intel.com>
2018-11-06 09:34:56 -0800

    Update sync servers

--- 3D_UNet/keras_training_only_version/sync_workers.sh
+++ 3D_UNet/keras_training_only_version/sync_workers.sh
@@ -2,19 +2,10 @@
 #rsync --progress -auzhe 'ssh' * aipg-demo03:/home/bduser/topologies/3D_UNet/keras_training_only_version/
 #rsync --progress -auzhe 'ssh' * aipg-demo04:/home/bduser/topologies/3D_UNet/keras_training_only_version/
 
-ssh demo02
-cd topologies
-git pull
-logout
+ssh demo02 "cd topologies && git pull"
 
-ssh demo03
-cd topologies
-git pull
-logout
+ssh demo03 "cd topologies && git pull"
 
-ssh demo04
-cd topologies
-git pull
-logout
+ssh demo04 "cd topologies && git pull"
 
 
