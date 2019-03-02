fab0352867ced31be7e7d8950a19681603d4e234
Tony Reina <37851530+tonyreina@users.noreply.github.com>
2019-03-02 11:57:29 -0800

    Update sync_workers.sh

--- 3D_UNet/keras_training_only_version/sync_workers.sh
+++ 3D_UNet/keras_training_only_version/sync_workers.sh
@@ -1,7 +1,3 @@
-#rsync --progress -auzhe 'ssh' * aipg-demo02:/home/bduser/topologies/3D_UNet/keras_training_only_version/ 
-#rsync --progress -auzhe 'ssh' * aipg-demo03:/home/bduser/topologies/3D_UNet/keras_training_only_version/
-#rsync --progress -auzhe 'ssh' * aipg-demo04:/home/bduser/topologies/3D_UNet/keras_training_only_version/
-
 ssh demo02 "cd topologies && git pull"
 
 ssh demo03 "cd topologies && git pull"
